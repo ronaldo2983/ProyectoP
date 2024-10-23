5ed09a2ab5c7aca1841054f994beba53f75f0955
Susana Castillo <castillosusana55@gmail.com>
2024-10-22 21:19:58 -0500

    first commit

---
+++ dcrm/mydb.py
@@ -0,0 +1,8 @@
+# Install Mysql on your computer
+# https://dev.mysql.com/downloads/installer/
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python
+
+
+import mysql.connector
